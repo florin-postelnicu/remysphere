73b16c47d632f372199b572248a8d41ddc18b91b
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-07-03 05:06:33 -0700

    Update RmiClient.java

--- Remmy/RmiClient.java
+++ Remmy/RmiClient.java
@@ -2,7 +2,7 @@ import java.rmi.Naming;
 
 public class RmiClient {
 public static void main(String args[]) throws Exception {
-        RmiServerIntf obj = (RmiServerIntf)Naming.lookup("//localhost/RmiServer");
-        System.out.println(obj.getMessage());
+        RmiServerIntf obj = (RmiServerIntf)Naming.lookup("//localhost/RmiServer");//The stub
+        System.out.println(obj.getMessage());//invocation of the remote method getMessage()
         }
         }
